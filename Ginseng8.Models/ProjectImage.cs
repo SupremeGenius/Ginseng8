b7944eedff6e3945c7f2a29f1813bdc792ba6ba1
Adam O'Neil <adamosoftware@gmail.com>
2019-04-05 12:53:37 -0400

    started on Designer Features, but not sure this will go anywhere. No harm adding this table

---
+++ Ginseng8.Models/ProjectImage.cs
@@ -0,0 +1,34 @@
+﻿using Ginseng.Models.Conventions;
+using Ginseng.Models.Interfaces;
+using Postulate.Base.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ginseng.Models
+{
+	/// <summary>
+	/// Screenshot or other visual content or info that's part of a project
+	/// </summary>
+	public class ProjectImage : BaseTable, IBody
+	{
+		[References(typeof(Project))]
+		public int ProjectId { get; set; }
+
+		[MaxLength(255)]
+		[Required]
+		public string Title { get; set; }
+
+		[MaxLength(255)]
+		[Required]
+		public string LargeImage { get; set; }
+
+		[MaxLength(255)]
+		[Required]
+		public string SmallImage { get; set; }
+
+		public string TextBody { get; set; }
+
+		public string HtmlBody { get; set; }
+
+		public int? Order { get; set; }
+	}
+}
